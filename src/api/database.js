c04f9e1af5970a8c8a2492474658e1922299f6a8
Robert Passafaro <Robert.Passafaro@gmail.com>
2020-07-01 17:43:03 -0700

    added firebase setting up api

---
+++ src/api/database.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyApn1dpI9L12MEB2Ib0IX9obJRmnNUrMt0",
+    authDomain: "truethought-1b13a.firebaseapp.com",
+    databaseURL: "https://truethought-1b13a.firebaseio.com",
+    projectId: "truethought-1b13a",
+    storageBucket: "truethought-1b13a.appspot.com",
+    messagingSenderId: "809873211146",
+    appId: "1:809873211146:web:9165b315cb88f563a444f3"
+  };
+
+  export const database = firebase.initializeApp(config);
+
+  
