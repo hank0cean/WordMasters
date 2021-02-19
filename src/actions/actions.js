24aef4617d0327cdbbfbc0550d0329f22a638f96
hank0cean <coreyjake.hansen@gmail.com>
2021-02-19 11:35:30 -0800

    basic redux init file structure added

---
+++ src/actions/actions.js
@@ -0,0 +1,15 @@
+import {LOG_IN, LOG_OUT} from './actionTypes';
+
+export const login = () => ({
+  type: LOG_IN,
+  payload: {
+
+  }
+});
+
+export const logout = () => ({
+  type: LOG_OUT,
+  payload: {
+
+  }
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import * as ActionTypes from './../actions'
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    isLogged,
+})
+
+export default rootReducer;
